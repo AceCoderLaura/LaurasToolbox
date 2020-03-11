1ebaee679136e7341f3aca34f30e8ee1a603f834
Laura Bailey <laurab@adaptsolutions.com.au>
2020-03-11 15:31:43 +1100

    Can stop the clock

--- Clockify.psm1
+++ Clockify.psm1
@@ -4,3 +4,19 @@ Set-Location $PSScriptRoot;
 
 # Get the Clockify API key
 $apiKey = Get-Content -Raw -Path "Clockify.key";
+
+function Stop-Clock
+{
+    # Get user info
+    $resp = Invoke-WebRequest -Headers @{ "X-Api-Key" = $apiKey } -ContentType "application/json" -Uri "https://api.clockify.me/api/v1/user"
+    $clockifyUser = ConvertFrom-Json $resp.Content;
+    $defaultWorkspace = $clockifyUser.defaultWorkspace;
+    $clockifyUserId = $clockifyUser.id;
+
+    # Set end time to now UTC/Zulu time format
+    $endTime = @{ end = [DateTime]::UtcNow.ToString("yyyy-MM-ddThh:mm:ssZ") };
+    $endTimeJson = ConvertTo-Json $endTime;
+
+    # Update the time entry on the server
+    Invoke-WebRequest -Headers @{ "X-Api-Key" = $apiKey } -ContentType "application/json" -Method PATCH -Uri "https://api.clockify.me/api/v1/workspaces/$defaultWorkspace/user/$clockifyUserId/time-entries" -Body $endTimeJson;
+}
