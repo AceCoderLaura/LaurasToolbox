eedf48e14e3f5fb5bf51b83d133d021adea9af52
Laura Bailey <laurab@adaptsolutions.com.au>
2021-03-25 14:16:04 +1100

    Can start the clock

--- Clockify.psm1
+++ Clockify.psm1
@@ -20,3 +20,29 @@ function Stop-Clock
     # Update the time entry on the server
     Invoke-WebRequest -Headers @{ "X-Api-Key" = $apiKey } -ContentType "application/json" -Method PATCH -Uri "https://api.clockify.me/api/v1/workspaces/$defaultWorkspace/user/$clockifyUserId/time-entries" -Body $endTimeJson;
 }
+
+function Start-Clock
+{
+	[CmdletBinding()]
+    param
+	(
+		[Parameter(Mandatory = $true)]
+		[string]$Description
+	)
+	
+	# Get user info
+    $resp = Invoke-WebRequest -Headers @{ "X-Api-Key" = $apiKey } -ContentType "application/json" -Uri "https://api.clockify.me/api/v1/user"
+    $clockifyUser = ConvertFrom-Json $resp.Content;
+    $defaultWorkspace = $clockifyUser.defaultWorkspace;
+	
+	# Set description and start time
+    $startInfo = @{ start = [DateTime]::UtcNow.ToString("yyyy-MM-ddThh:mm:ssZ"); description = $Description };
+    $startInfoJson = ConvertTo-Json $startInfo;
+
+	# Start timer on the server
+	Invoke-WebRequest -Headers @{ "X-Api-Key" = $apiKey } `
+					  -ContentType "application/json" `
+					  -Method POST `
+					  -Uri "https://api.clockify.me/api/v1/workspaces/$defaultWorkspace/time-entries" `
+					  -Body $startInfoJson;
+}
